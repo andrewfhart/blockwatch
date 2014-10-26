ebaa438a9060c11dc82934c6bc913904daf210b6
Andrew Hart <andrew@pogoseat.com>
2014-10-26 12:11:40 -0700

    Remove unsupported search types

--- src/js/controllers/search.js
+++ src/js/controllers/search.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('blockwatch.search').controller('SearchController',
-  function($scope, $routeParams, $location, $timeout, Global, Block, Transaction, Address, BlockByHeight) {
+  function($scope, $routeParams, $location, $timeout, Global, Block, BlockByHeight) {
   $scope.global = Global;
   $scope.loading = false;
 
@@ -28,36 +28,22 @@ angular.module('blockwatch.search').controller('SearchController',
     }, function() {
       _resetSearch();
       $location.path('block/' + q);
-    }, function() { //block not found, search on TX
-      Transaction.get({
-        txId: q
-      }, function() {
-        _resetSearch();
-        $location.path('tx/' + q);
-      }, function() { //tx not found, search on Address
-        Address.get({
-          addrStr: q
-        }, function() {
+    }, function() { // block by height not found
+      if (isFinite(q)) { // ensure that q is a finite number. A logical height value.
+        BlockByHeight.get({
+          blockHeight: q
+        }, function(hash) {
           _resetSearch();
-          $location.path('address/' + q);
-        }, function() { // block by height not found
-          if (isFinite(q)) { // ensure that q is a finite number. A logical height value.
-            BlockByHeight.get({
-              blockHeight: q
-            }, function(hash) {
-              _resetSearch();
-              $location.path('/block/' + hash.blockHash);
-            }, function() { //not found, fail :(
-              $scope.loading = false;
-              _badQuery();
-            });
-          }
-          else {
-            $scope.loading = false;
-            _badQuery();
-          }
+          $location.path('/block/' + hash.blockHash);
+        }, function() { //not found, fail :(
+          $scope.loading = false;
+          _badQuery();
         });
-      });
+      }
+      else {
+        $scope.loading = false;
+        _badQuery();
+      }
     });
   };
 });
